4312b53849836a9409543ea684e777062c0028e3
Raul Sposito <raulsposito@gmail.com>
2019-10-29 01:32:56 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,4 +10,5 @@ end
 create_project_hash
 
 # projects: kickstarter.css("li.project.grid_4")
-# project.css("h2.bbcard_name strong a").text
+# projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text
