b629158e5a26f6b588dc547e8dd74dadbbe853ea
Raul Sposito <raulsposito@gmail.com>
2019-10-29 01:40:05 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,16 +9,12 @@ require 'pry'
 # description: project.css("p.bbcard_blurb").text
 # location: project.css("ul.project-meta span.location-name").text
 # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
- 
-def create_project_hash
-  html = File.read('fixtures/kickstarter.html')
-  kickstarter = Nokogiri::HTML(html)
- 
+ def create_project_hash
   projects = {}
  
-  # Iterate through the projects
   kickstarter.css("li.project.grid_4").each do |project|
-    projects[project] = {}
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
   end
  
   # return the projects hash
